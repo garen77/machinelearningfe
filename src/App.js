2843803961128fef425f7123d76d821cbb3b8c0c
creo.cons <crescenzo.garofalo@gmail.com>
2023-09-18 22:38:38 +0200

    added mobilenet load

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from './components/NavBar'
 import Home from './pages';
@@ -6,17 +6,45 @@ import About from "./pages/about";
 import ImageRecognition from "./pages/imgRecognition";
 import './styles/appStyle.scss';
 
+import "@tensorflow/tfjs";
+import * as mobileNet from "@tensorflow-models/mobilenet";
+import { mod } from "@tensorflow/tfjs";
+import LoadingSpinner from "./components/Spinner";
+
 const App = () => {
+
+  const [model, setModel] = useState(null);
+
+  useEffect(() => {
+    const loadModel = async () => {
+      const model = await mobileNet.load();
+      setModel(model);
+    };
+    loadModel();
+  },[]);
+
   return (
   
-    <BrowserRouter>
-    <NavBar />
-      <Routes>
-        <Route path="/" exact element={ <Home /> }/>
-        <Route path="/imageRecognition" element={ <ImageRecognition /> } />
-        <Route path="/about" element={ <About /> } />
-      </Routes>
-    </BrowserRouter>
+    <>
+    {
+      !model ? (
+          <div className='container'>
+            <LoadingSpinner message={"Loading mobilenet model..."} />
+          </div>
+        ) 
+      : (
+          <BrowserRouter>
+          <NavBar />
+            <Routes>
+              <Route path="/" exact element={ <Home /> }/>
+              <Route path="/imageRecognition" element={ <ImageRecognition /> } />
+              <Route path="/about" element={ <About /> } />
+            </Routes>
+          </BrowserRouter>
+      )
+    }
+    </>
+
   );
 };
 
--- src/pages/imgRecognition.js
+++ src/pages/imgRecognition.js
@@ -1,22 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import CustomFileInput from '../components/CustomFileInput';
 
 
-import "@tensorflow/tfjs";
-import * as mobileNet from "@tensorflow-models/mobilenet";
-import LoadingSpinner from '../components/Spinner';
-
 const ImageRecognition = () => {
 
-  const [model, setModel] = useState(null);
 
-  useEffect(() => {
-    const loadModel = async () => {
-      const model = await mobileNet.load();
-      setModel(model);
-    };
-    loadModel();
-  },[]);
 
   const onImageChange = async () => {
     
@@ -26,9 +14,7 @@ const ImageRecognition = () => {
     <>
       <div className='container'>
         <h1>Image Recognition</h1>
-        {
-          !model ? (<LoadingSpinner message={"Loading mobilenet model..."} />) : (<CustomFileInput />)
-        }  
+        <CustomFileInput />
       </div>
       
     </>
