b9a6c3797157d3e39384180050256d3e0556423d
creo.cons <crescenzo.garofalo@gmail.com>
2023-09-28 22:32:33 +0200

    mobile fixes

--- src/components/Popup/index.js
+++ src/components/Popup/index.js
@@ -3,12 +3,11 @@ import "./Popup.scss";
 export const Popup = (props) => {
   return (
     <div className="popup-container">
-     <div className="popup-body">
-     <button onClick={props.closePopup} className="popup-close-button">X</button>
-      {props.text && <h1>{props.text}</h1>}
-      { props.children }
-      
-     </div>
+      <button className="popup-x" onClick={props.closePopup} >X</button>
+      <div className="popup-body">
+        {props.text && <h1>{props.text}</h1>}
+        { props.children }
+      </div>
     </div>
   );
 };
